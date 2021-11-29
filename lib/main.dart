3e6c13153d9fe48b6b6342b6a22faea4818acad7
newchallenger082 <newchallenger082@gmail.com>
2021-11-29 23:27:31 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Haloooo',
+          style: GoogleFonts.poppins(
+            fontSize: 50,
+          ),
+        ),
+      ),
+    );
+  }
+}
